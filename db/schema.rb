a64d69267a8730ac3c79f81c326603016f7b6e74
Nick <nsousanis14@yahoo.com>
2020-01-21 20:44:31 -0600

    Updated seed file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_20_011029) do
+ActiveRecord::Schema.define(version: 2020_01_21_031511) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,6 +71,14 @@ ActiveRecord::Schema.define(version: 2019_12_20_011029) do
     t.string "race_type"
     t.date "date"
     t.integer "city_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "shoes", force: :cascade do |t|
+    t.string "name"
+    t.integer "user_id"
+    t.decimal "distance", precision: 5, scale: 2
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
